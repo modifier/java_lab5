1ec178c4b463193417ac6f0587419adb2542944b
Eugenio <the.modifikator@gmail.com>
2013-05-27 14:40:49 +0400

    Set locale via console

--- src/MainController.java
+++ src/MainController.java
@@ -12,7 +12,8 @@ import java.awt.event.MouseEvent;
 public class MainController {
     public float[] y_values = new float[]{-4, 2, 4};
     public float[] x_values = new float[]{-3, -2, 1, 2, 5};
-
+    
+    private ListResourceBundle locale;
     private MarkCollection points;
 
     private Graphic graphic;
@@ -21,10 +22,23 @@ public class MainController {
      * Launch the application.
      */
     public static void main(String[] args) {
+        final ListResourceBundle locale;
+        if(args.length > 0) {
+            if(args[0].equals("eo")) {
+                locale = new Locale_eo();
+            } else if(args[0].equals("en")) {
+                locale = new Locale_en();
+            } else {
+                System.out.println("Unknown locale");
+                return
+            }
+        } else {
+            locale = new Locale_en();
+        }
         EventQueue.invokeLater(new Runnable() {
             public void run() {
                 try {
-                    MainController controller = new MainController();
+                    MainController controller = new MainController(locale);
                 } catch (Exception e) {
                     e.printStackTrace();
                 }
@@ -32,7 +46,8 @@ public class MainController {
         });
     }
 
-    public MainController() {
+    public MainController(ListResourceBundle locale) {
+        this.locale = locale;
         points = new MarkCollection();
         points.setRadius(20);
 
@@ -40,7 +55,7 @@ public class MainController {
     }
 
     private void initializeWindow() {
-        MainView window = new MainView();
+        MainView window = new MainView(locale);
         window.setVisible();
 
         window.setPoints(points);
